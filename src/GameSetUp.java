0417931080f32f914bb54eb03a41acdcd3a5ec00
Venus Poon <venuspsw@hotmail.com>
2023-10-27 13:12:22 +0200

    add comments and hardcode gameNrList

--- src/GameSetUp.java
+++ src/GameSetUp.java
@@ -1,19 +1,15 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 
 public class GameSetUp {
     String[][] gameFrame;
     ArrayList<String> gameNrList;
 
+    //create a shuffled list of numbers for 2D gaming frame
     public void setGameNrList(){
-        ArrayList<String> temp = new ArrayList<>();
-        for (int i = 0; i < 16; i++) {
-            if (i == 0){
-                temp.add(" ");
-            } else {
-                temp.add(String.valueOf(i));
-            }
-        }
+        ArrayList<String> temp = new ArrayList<>
+                (Arrays.asList(" ","1","2","3","4","5","6","7","8","9","10","11","12","13","14","15"));
         Collections.shuffle(temp);
         this.gameNrList = new ArrayList<>(temp);
     }
@@ -28,6 +24,14 @@ public class GameSetUp {
         }
     }
 
+    public GameSetUp(){
+        gameFrame = new String[4][4];
+        gameNrList = new ArrayList<>();
+        setGameNrList();
+        setGameFrameValue();
+    }
+
+    //Test mode: update 2D gaming frame with test data
     public void setTestGameFrameValue(String[][] inputFrame){
         int counter = 0;
         for (int i = 0; i < inputFrame.length; i++) {
@@ -39,13 +43,7 @@ public class GameSetUp {
         setGameFrameValue();
     }
 
-    public GameSetUp(){
-        gameFrame = new String[4][4];
-        gameNrList = new ArrayList<>();
-        setGameNrList();
-        setGameFrameValue();
-    }
-
+    //print the 2D gaming frame
     public String toString(){
         String temp = "";
         for (int i = 0; i < gameFrame.length; i++) {
@@ -57,6 +55,7 @@ public class GameSetUp {
         return temp;
     }
 
+    //get index of the selected number in 2D gaming frame
     public int[] getIndexOfSelectedNr(String selectedNr){
         int row = -1;
         int column = -1;
@@ -71,25 +70,31 @@ public class GameSetUp {
         return new int[]{row,column};
     }
 
+    //start the game and pick a selected number
     public void goSelectNr(String selectedNr){
         if (checkIfSelectedNrNextToEmpty(selectedNr)){
             int indexOfEmpty = getIndex(" ");
             int indexOfSelectedNr = getIndex(selectedNr);
+            //exchange locations of the selected number and empty space
             gameNrList.set(indexOfSelectedNr," ");
             gameNrList.set(indexOfEmpty,selectedNr);
             setGameFrameValue();
+
+            //print msg if the game is finished.
             if (isGameNrInOrder()){
                 System.out.println("You win!");
             }
         }
     }
 
+    //check in overall if there's empty ABOVE,BELOW,on LEFT,on RIGHT side of the selected number
     public boolean checkIfSelectedNrNextToEmpty(String selectedNr){
         int[] temp = getIndexOfSelectedNr(selectedNr);
         return ifSelectedNrAboveIsEmpty(temp) || ifSelectedNrBelowIsEmpty(temp)
                 || ifSelectedNrLeftIsEmpty(temp) || ifSelectedNrRightIsEmpty(temp);
     }
 
+    //check if there's an empty space ABOVE the selected number
     public boolean ifSelectedNrAboveIsEmpty(int[] indexOfselectedNr){
         int row = indexOfselectedNr[0];
         int column = indexOfselectedNr[1];
@@ -101,6 +106,7 @@ public class GameSetUp {
         return false;
     }
 
+    //check if there's an empty space BELOW the selected number
     public boolean ifSelectedNrBelowIsEmpty(int[] indexOfselectedNr){
         int row = indexOfselectedNr[0];
         int column = indexOfselectedNr[1];
@@ -112,6 +118,7 @@ public class GameSetUp {
         return false;
     }
 
+    //check if there's an empty space on the LEFT hand side of the selected number
     public boolean ifSelectedNrLeftIsEmpty(int[] indexOfselectedNr){
         int row = indexOfselectedNr[0];
         int column = indexOfselectedNr[1];
@@ -123,6 +130,7 @@ public class GameSetUp {
         return false;
     }
 
+    //check if there's an empty space on the RIGHT hand side of the selected number
     public boolean ifSelectedNrRightIsEmpty(int[] indexOfselectedNr){
         int row = indexOfselectedNr[0];
         int column = indexOfselectedNr[1];
@@ -134,6 +142,7 @@ public class GameSetUp {
         return false;
     }
 
+    //get index of desired numbers in the arraylist of game numbers
     public int getIndex(String nr){
         for (int i = 0; i < gameNrList.size(); i++) {
             if (gameNrList.get(i).equals(nr)){
@@ -143,6 +152,7 @@ public class GameSetUp {
         throw new NullPointerException("No such number");
     }
 
+    //check if the numbers are in order
     public boolean isGameNrInOrder(){
         int correctCount = 0;
         for (int i = 1; i < 16; i++) {
